8dad6f22cb62339cfe7f4d2c19e4d089492cf436
letmego1st <imcoding4real@gmail.com>
2023-06-03 06:12:04 -0700

    added thought-routes info

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require('../../controllers/thought-controller.js');
+
+// /api/thoughts
+router.route('/').get(getAllThought).post(createThought);
+
+// /api/thoughts/:id
+router
+  .route('/:courseId')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+  // /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
