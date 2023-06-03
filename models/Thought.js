38dfb69bfc299e0633eaeabf4d713c64477e7c48
letmego1st <imcoding4real@gmail.com>
2023-06-03 04:53:15 -0700

    added Thought.js file in models

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create a thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      // Must be between 1 and 280 characters long
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
